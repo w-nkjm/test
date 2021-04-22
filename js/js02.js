677d6b9d87a79e5956e53dd8aac4869f82498fd2
w-nkjm <neo.colza93@gmail.com>
2021-04-22 09:43:23 +0900

    修正

--- js/js02.js
+++ js/js02.js
@@ -1,39 +1,40 @@
 window.onload = function() {
     scroll_effect();
 
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         scroll_effect();
     });
 
-    function scroll_effect(){
-        $('.fadein').each(function(){
+    function scroll_effect() {
+        $('.fadein').each(function() {
             var elemPos = $(this).offset().top;
             var scroll = $(window).scrollTop();
             var windowHeight = $(window).height();
-            if (scroll > elemPos - windowHeight){
+            if (scroll > elemPos - windowHeight) {
                 $(this).addClass('scrollin');
             }
         });
     }
 };
 
-$(function(){
+$(function() {
     $('.humburger-btn').on('click',
         function() {
             $(this).toggleClass('active');
             $('h1').toggle();
-            $('.gnav').fadeToggle(1000);
+            $('.gnav').toggleClass('slide-in');
             $('.nav-menu').toggleClass('active');
-            
-            
-        });
+            $('body').toggleClass('noscrol');
+    });
 });
 
 $(function() {
-    $('.line-effect').on('click',
+    $('.gr').on('click',
         function() {
-            $('.gnav').removeAttr('style');
+            // $('.gnav').removeAttr('class');
             $('.humburger-btn').removeClass('active');
             $('.nav-menu').removeClass('active');
+            $('body').removeClass('noscrol');
         });
 });
+
